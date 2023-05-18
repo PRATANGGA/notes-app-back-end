d533089549a1995ef65b8901044ef9e594fcbf86
PRATANGGA <pratanggajulang31@gmail.com>
2023-05-18 13:43:02 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,16 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+const notes = [
+    {    
+      id: "1",    
+      title: "Belanja",   
+      createdAt: "2023-05-13",    
+      updatedAt: "2023-05-13",    
+      tags: ["pribadi", "belanja"],    
+      body: "Beli susu, roti, dan telur di supermarket."  
+    },  
+  ]; 
+
+module.exports = notes;
